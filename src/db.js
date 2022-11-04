cde99844b3b5a3b2dcec72b732e2cfaac49bb303
Ferrama <frmachado1290@gmail.com>
2022-11-04 19:22:53 -0300

    configs

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const { DB_CONNECT } = require('./config');
 
 
 const sequelize = new Sequelize(
-  `${process.env.DB_CONNECT}`,
+  `${DB_CONNECT}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
